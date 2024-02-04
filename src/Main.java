42b49c29d7fadb36de3e9fcce6536f38b93ac26a
Денис Колесников <73371887@mail.ru>
2024-02-04 15:32:02 +0300

    Homework10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework");
+
+    //1
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = " Ivanov";
+        String result = lastName +  firstName +  middleName;
+        System.out.println("ФИО сотрудника - " + result);
+    //2
+        String fullname2 = "Ivanov Ivan Ivanovich";
+        System.out.println(fullname2.toUpperCase());
+    //3
+       String fullName = "Иванов Семён Семёнович";
+       String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println(fullNameTrue);
+
     }
 }
